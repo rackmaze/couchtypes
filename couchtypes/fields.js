758af58d4738d550027ad6831a5f32dbf3ee3cb6
Kirill Pertsev <kika@rackmaze.com>
2013-11-23 16:39:54 -0800

    Support functional required: field for embedded types

--- couchtypes/fields.js
+++ couchtypes/fields.js
@@ -243,7 +243,13 @@ Embedded.prototype.validate = function (doc, value, raw) {
     // don't validate empty fields, but check if required
     if (this.isEmpty(value, raw)) {
         if (this.required) {
-            return [ new Error('Required field') ];
+            if(_.isFunction(this.required)) {
+                if(this.required(doc, value, raw)) {
+                    return [ new Error('Required field') ];
+                }
+            } else {
+                return [ new Error('Required field') ];
+            }
         }
         return [];
     }
